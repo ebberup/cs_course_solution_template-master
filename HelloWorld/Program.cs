01d84469af939373fce1dc9c8bd196e27e130789
henning <empty>
2017-08-29 14:55:02 +0200

    Implementering af assignment

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast navn");
+            var navn = Console.ReadLine();
+            Console.WriteLine("Velkommen "+navn.ToUpper());
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
