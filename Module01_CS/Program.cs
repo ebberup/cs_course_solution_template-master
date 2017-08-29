3f68b2079588bc05b6ce5d35a663a0d875de5c0b
henning <empty>
2017-08-29 14:31:37 +0200

    Added call to DebugDemo

---
+++ Module01_CS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module01_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world");
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
